cb5a45db311cefe44886d62a78adbb7e32b6cabf
djinodissingar <dissingardjino@icloud.com>
2023-01-09 16:23:48 +0100

    - Mise en place de la localisation (2)

--- lib/controller/DetailViewController.dart
+++ lib/controller/DetailViewController.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:untitled/model/my_user.dart';
 
 class DetailViewController extends StatefulWidget {
@@ -13,17 +14,58 @@ class _DetailViewControllerState extends State<DetailViewController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: bodyPage(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: (){
+                print("Je clique mon favoris");
+              },
+              icon: const FaIcon(FontAwesomeIcons.heartCircleCheck,color: Colors.red,)
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: bodyPage(),
+      ),
     );
   }
 
   Widget bodyPage(){
-    //Nom et le prénom de la personne
+
     //Son pseudo
-    // Son avatar
     // bouton cliquable pour Tchatter
     //Bouton cliquable pour ajouter en favoris l'utilisateur
 
-    return Text("Je suis dans la nouvlle page");
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 70,
+            backgroundImage: NetworkImage(widget.monUtilisateur.avatar!),
+          ),
+          Text(widget.monUtilisateur.mail,style: const TextStyle(fontSize: 25,color: Colors.black),),
+          Text(widget.monUtilisateur.fullName,style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.purple),),
+          Text(widget.monUtilisateur.pseudo!,style: const TextStyle(fontSize: 15,color: Colors.purple,fontStyle: FontStyle.italic),),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 30.0,
+                shape: const StadiumBorder(),
+                backgroundColor: Colors.amber
+
+
+              ),
+                onPressed: (){
+                  print("Je veux chatter");
+                },
+                child: Text("Chat")
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
